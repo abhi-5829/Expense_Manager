4fd85df35bc89eaa6ad5bc2c866cc1df298652f6
abhi-5829 <abhishekbisi321@gmail.com>
2023-03-24 22:49:29 +0530

    Date formatted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import './transactions.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -14,13 +15,61 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   @override
+  final List<Transactions> transactions = [
+    Transactions(
+        id: 'A212', title: 'Shampoo', amount: 423, ptime: DateTime.now()),
+    Transactions(
+        id: 'A213', title: 'Conditioner', amount: 225, ptime: DateTime.now()),
+  ];
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Expense Manger'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+       // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(),
+          Column(
+
+            children: transactions.map((tx) {
+              return Card(
+                  child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    child: Text(
+                      "${tx.amount}",
+                      style: TextStyle(color: Colors.purple.shade300),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                    decoration: BoxDecoration(
+                      
+                      border: Border.all(
+                        color: Colors.purple.shade400,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(tx.ptime),
+                        style: TextStyle(color: Colors.grey.shade600),
+                      ),
+                    ],
+                  ),
+                ],
+              ));
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
