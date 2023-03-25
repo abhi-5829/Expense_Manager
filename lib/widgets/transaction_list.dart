29055ec9bfe4ffe0f95a01bfd366046444c4a36e
abhi-5829 <abhishekbisi321@gmail.com>
2023-03-26 02:55:59 +0530

    lecutre 91
    ListView added int transaction_list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,43 +11,41 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height:400,
-      child: SingleChildScrollView(
-        child: Column(
-            children: transactions.map((tx) {
-          return Card(
-              child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                child: Text(
-                  "${tx.amount}",
-                  style: TextStyle(color: Colors.purple.shade300),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple.shade400,
-                    width: 2,
-                  ),
+      child: ListView(
+          children: transactions.map((tx) {
+        return Card(
+            child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              child: Text(
+                "${tx.amount}",
+                style: TextStyle(color: Colors.purple.shade300),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.purple.shade400,
+                  width: 2,
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    tx.title,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    DateFormat.yMMMd().format(tx.ptime),
-                    style: TextStyle(color: Colors.grey.shade600),
-                  ),
-                ],
-              ),
-            ],
-          ));
-        }).toList()),
-      ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  tx.title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  DateFormat.yMMMd().format(tx.ptime),
+                  style: TextStyle(color: Colors.grey.shade600),
+                ),
+              ],
+            ),
+          ],
+        ));
+      }).toList()),
     );
   }
 }
