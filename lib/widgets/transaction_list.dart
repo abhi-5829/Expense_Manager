085e67413253f22bd6568ce70a17fd0a2fa98dd4
abhi-5829 <abhishekbisi321@gmail.com>
2023-03-26 03:01:52 +0530

    lecture 91
    ListView.builder() added int transaction_list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,49 +3,50 @@ import '../models/transactions.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
- final List<Transactions> transactions;
+  final List<Transactions> transactions;
 
- const TransactionList({required this.transactions});
+  const TransactionList({required this.transactions});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height:400,
-      child: ListView(
-          children: transactions.map((tx) {
-        return Card(
-            child: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-              child: Text(
-                "${tx.amount}",
-                style: TextStyle(color: Colors.purple.shade300),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.purple.shade400,
-                  width: 2,
-                ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        height: 400,
+        child: ListView.builder(
+          itemBuilder: (context, i) {
+            return Card(
+                child: Row(
               children: [
-                Text(
-                  tx.title,
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  child: Text(
+                    "${transactions[i].amount}",
+                    style: TextStyle(color: Colors.purple.shade300),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple.shade400,
+                      width: 2,
+                    ),
+                  ),
                 ),
-                Text(
-                  DateFormat.yMMMd().format(tx.ptime),
-                  style: TextStyle(color: Colors.grey.shade600),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transactions[i].title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[i].ptime),
+                      style: TextStyle(color: Colors.grey.shade600),
+                    ),
+                  ],
                 ),
               ],
-            ),
-          ],
+            ));
+          },
+          itemCount: transactions.length,
         ));
-      }).toList()),
-    );
   }
 }
